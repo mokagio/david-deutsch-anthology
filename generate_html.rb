740fc6253d31d15e5392f95f9af03968200adef9
Gio Lodi <gio@mokacoding.com>
2024-10-09 09:39:02 +0100

    Add script to generate HTML for the data

---
+++ generate_html.rb
@@ -0,0 +1,26 @@
+require 'erb'
+require 'yaml'
+
+# Load data from YAML file
+data = YAML.load_file('list.yml')
+
+# Extract the sections from the YAML file
+books = data['books']
+talks = data['talks']
+podcast_interviews = data['podcast_interviews']
+other = data['other']
+
+# Load the ERB template
+template = File.read('template.erb')
+
+# Bind the variables and generate the HTML
+erb = ERB.new(template)
+html_content = erb.result(binding)
+
+# Save the generated HTML to a file
+name = 'index.html'
+File.open(name, 'w') do |file|
+  file.write(html_content)
+end
+
+puts "HTML page generated successfully: #{name}"
